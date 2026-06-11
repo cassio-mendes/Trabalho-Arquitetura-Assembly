b894648519905619201b6ed4618c45eb9979b871
cassio-mendes <133818577+cassio-mendes@users.noreply.github.com>
2026-06-11 19:10:03 -0300

    Implementando função de cadastro

--- TrabalhoArquitetura.asm
+++ TrabalhoArquitetura.asm
@@ -17,13 +17,17 @@
 	mensagemOpcao: .asciiz "\nOpção: "
 	mensagemErroOpcao: .asciiz "ERRO: opção inválida"
 	mensagemTitulo: .asciiz "\nDigite o título do livro: "
-	mensagemCodigo: .asciiz " - Código "
-	mensagemSucessoCadastro: .asciiz "Livro cadastrado com sucesso!"
+	mensagemCodigo: .asciiz "\nCódigo "
+	mensagemTraco: .asciiz " - "
+	mensagemSucessoCadastro: .asciiz "\nLivro cadastrado com sucesso!\n"
 	
 	#Título inserido pelo usuário
 	tituloLivro: .space 51
 		
 .text
+	li $s0, 0	#Registrador que contém o código do próximo número a ser cadastrado
+	li $s1, 0	#Registrador que contém o índice dos arrays codigos e status
+	
 	jal printMenu
 	
 	loopPrincipal:
@@ -52,26 +56,43 @@
 		la $a0, mensagemErroOpcao
 		jal printString
 		
-		jal printMenu	#Reinicia o loop principal
+		jal loopPrincipal #Reinicia o loop principal
 		
 		cadastrar:	#Função para cadastro de novos livros
 			la $a0, mensagemTitulo
 			jal printString
 			jal lerTitulo
 			
-			la $a0, tituloLivro
-			jal printString
+			#Cadastro nos arrays
+			sw $s0, codigos($s1)
+			sw $zero, status($s1)
+			
+			#Atualização do código e dos índices
+			addi $s0, $s0, 1
+			addi $s1, $s1, 1
 			
 			la $a0, mensagemCodigo
 			jal printString
 			
+			move $a0, $s0
+			jal printInteiro
 			
+			la $a0, mensagemTraco
+			jal printString
+			
+			la $a0, tituloLivro
+			jal printString
+			
+			la $a0, mensagemSucessoCadastro
+			jal printString
+			
+			jal loopPrincipal #Reinicia o loop principal
 		
-		emprestar:
+		emprestar:	#Função para empréstimo de livros que estejam disponíveis
 		
-		devolver:
-		
-		listar:
+		devolver:	#Função para devolução de livros emprestados
+			
+		listar:		#Função para listagem de todos os livros cadastrados e seus respectivos status
 	
 	printString:
 		li $v0, 4
@@ -94,6 +115,12 @@
 	lerTitulo:
 		li $v0, 8
 		la $a0, tituloLivro
+		li $a1, 51
+		syscall
+		jr $ra
+		
+	printInteiro:
+		li $v0, 1
 		syscall
 		jr $ra
 
